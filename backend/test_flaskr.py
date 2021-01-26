6b82bf3a8b78ecefe64a9156fa3097849ec8933a
AbdullahHB <abdullahhesham@protonmail.com>
2021-01-26 13:05:44 +0200

    Refactor and format code
    - Make usage of .env file for app.
    - Update setup db to have custom user, pass, database etc, and default to SQLite in memory
    -  Update tests to use db test name, and create all tables before testing, and dropping all after finishing.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -3,8 +3,8 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
-from models import setup_db, Question, Category
+from .flaskr import create_app
+from .models import setup_db, Question, Category
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -12,22 +12,24 @@ class TriviaTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
-        self.app = create_app()
+        self.app = create_app(test_config={
+            'ENV': 'TESTING',
+            'TESTING': True,
+        })
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        setup_db(self.app, self.database_path)
 
         # binds the app to the current context
         with self.app.app_context():
-            self.db = SQLAlchemy()
-            self.db.init_app(self.app)
+            self.db = setup_db(self.app, {
+                'db_name': os.getenv('DB_TEST_NAME')
+            })
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
-        pass
+        # drop all tables after finishing
+        self.db.drop_all()
 
     """
     TODO
