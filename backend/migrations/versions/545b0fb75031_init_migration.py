5a9b7ab9d59f5a920a0020a38bcaa3a4cbf7e93b
AbdullahHB <abdullahhesham@protonmail.com>
2021-01-31 00:35:49 +0200

    Update migrations to make sure it insert questions with IDs for testing

--- backend/migrations/versions/545b0fb75031_init_migration.py
+++ backend/migrations/versions/545b0fb75031_init_migration.py
@@ -51,24 +51,28 @@ def upgrade():
 
     default_questions = [
         {
+            "id": 1,
             'question': "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?",
             'answer': "Maya Angelou",
             'difficulty': 2,
             'category_id': 4,
         },
         {
+            "id": 2,
             'question': "What boxer's original name is Cassius Clay?",
             'answer': "Muhammad Ali",
             'difficulty': 1,
             'category_id': 4,
         },
         {
+            "id": 3,
             'question': "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?",
             'answer': "Apollo 13",
             'difficulty': 4,
             'category_id': 5,
         },
         {
+            "id": 4,
             'question': "What actor did author Anne Rice first denounce, "
                         "then praise in the role of her beloved Lestat?",
             'answer': "Tom Cruise",
@@ -76,6 +80,7 @@ def upgrade():
             'category_id': 5,
         },
         {
+            "id": 5,
             'question': "What was the title of the 1990 fantasy directed by Tim Burton about a young man with "
                         "multi-bladed appendages?",
             'answer': "Edward Scissorhands",
@@ -83,60 +88,70 @@ def upgrade():
             'category_id': 5,
         },
         {
+            "id": 6,
             'question': "Which is the only team to play in every soccer World Cup tournament?",
             'answer': "Brazil",
             'difficulty': 3,
             'category_id': 6,
         },
         {
+            "id": 7,
             'question': "Which country won the first ever soccer World Cup in 1930?",
             'answer': "Uruguay",
             'difficulty': 4,
             'category_id': 6,
         },
         {
+            "id": 8,
             'question': "Who invented Peanut Butter?",
             'answer': "George Washington Carver",
             'difficulty': 2,
             'category_id': 4,
         },
         {
+            "id": 9,
             'question': "What is the largest lake in Africa?",
             'answer': "Lake Victoria",
             'difficulty': 2,
             'category_id': 3,
         },
         {
+            "id": 10,
             'question': "In which royal palace would you find the Hall of Mirrors?",
             'answer': "The Palace of Versailles",
             'difficulty': 3,
             'category_id': 3,
         },
         {
+            "id": 11,
             'question': "The Taj Mahal is located in which Indian city?",
             'answer': "Agra",
             'difficulty': 2,
             'category_id': 3,
         },
         {
+            "id": 12,
             'question': "Which Dutch graphic artist–initials M C was a creator of optical illusions?",
             'answer': "Escher",
             'difficulty': 1,
             'category_id': 2,
         },
         {
+            "id": 13,
             'question': "La Giaconda is better known as what?",
             'answer': "Mona Lisa",
             'difficulty': 3,
             'category_id': 2,
         },
         {
+            "id": 14,
             'question': "How many paintings did Van Gogh sell in his lifetime?",
             'answer': "One",
             'difficulty': 4,
             'category_id': 2,
         },
         {
+            "id": 15,
             'question': "Which American artist was a pioneer of Abstract Expressionism, and a leading exponent of "
                         "action painting?",
             'answer': "Jackson Pollock",
@@ -144,24 +159,28 @@ def upgrade():
             'category_id': 2,
         },
         {
+            "id": 16,
             'question': "What is the heaviest organ in the human body?",
             'answer': "The Liver",
             'difficulty': 4,
             'category_id': 1,
         },
         {
+            "id": 17,
             'question': "Who discovered penicillin?",
             'answer': "Alexander Fleming",
             'difficulty': 3,
             'category_id': 1,
         },
         {
+            "id": 18,
             'question': "Hematology is a branch of medicine involving the study of what?",
             'answer': "Blood",
             'difficulty': 4,
             'category_id': 1,
         },
         {
+            "id": 19,
             'question': "Which dung beetle was worshipped by the ancient Egyptians",
             'answer': "Scarab",
             'difficulty': 4,
